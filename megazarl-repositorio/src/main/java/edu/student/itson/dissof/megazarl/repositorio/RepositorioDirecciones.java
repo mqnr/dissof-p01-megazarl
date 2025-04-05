2be2c14a38b8f320694409c841e0e58d2f8f293f
mzamorano <martin@mzamorano.com>
2025-04-04 23:59:43 -0700

    dejar de trackear módulos innecesarios a v1

--- megazarl-repositorio/src/main/java/edu/student/itson/dissof/megazarl/repositorio/RepositorioDirecciones.java
+++
@@ -1,6 +0,0 @@
-package edu.student.itson.dissof.megazarl.repositorio;
-
-import edu.student.itson.dissof.megazarl.dto.modelos.Direccion;
-
-public interface RepositorioDirecciones extends Repositorio<Direccion> {
-}
--- megazarl-servicios/src/main/java/edu/student/itson/dissof/megazarl/servicios/RegistroCodigosPostales.java
+++
@@ -1,8 +0,0 @@
-package edu.student.itson.dissof.megazarl.servicios;
-
-import edu.student.itson.dissof.megazarl.dto.DetallesDerivadosDireccionDTO;
-
-public interface RegistroCodigosPostales {
-
-    DetallesDerivadosDireccionDTO obtenerDetalles(String codigoPostal);
-}
