0dd3250124ae101e0b0be4dd0d8622393c304dd7
mzamorano <martin@mzamorano.com>
2025-04-08 01:24:08 -0700

    arreglar nombre de atributo en ConfiguracionFuentes

--- megazarl-configuracion/src/main/java/edu/student/itson/dissof/megazarl/configuracion/ConfiguracionFuentes.java
+++ megazarl-configuracion/src/main/java/edu/student/itson/dissof/megazarl/configuracion/ConfiguracionFuentes.java
@@ -1,12 +1,12 @@
 package edu.student.itson.dissof.megazarl.configuracion;
 
 public record ConfiguracionFuentes(
-        FuenteDatos fuenteCliente
+        FuenteDatos cliente
 ) {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        builder.append("Fuente cliente: ").append(fuenteCliente);
+        builder.append("Fuente cliente: ").append(cliente);
         return builder.toString();
     }
 }
